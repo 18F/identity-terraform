6244748b1637467a9226d49928cc2c56cdd4cacd
Andy Brody <andy.brody@beta.dhs.gov>
2018-06-19 19:53:31 -0400

    Add a module for creating launch templates.

---
+++ launch_template/main.tf
@@ -0,0 +1,84 @@
+variable "role" {
+}
+
+variable "env" {
+  description = "Environment name"
+}
+
+variable "root_domain" {
+}
+
+variable "ami_id_map" {
+  description = "Mapping from role names to AMI IDs"
+  type = "map"
+}
+
+variable "default_ami_id" {
+  description = "AMI ID to use if the role is not found in the map"
+}
+
+variable "instance_type" {
+}
+
+variable "instance_initiated_shutdown_behavior" {
+  default = "terminate"
+}
+
+variable "iam_instance_profile_name" {
+}
+
+variable "user_data" {
+}
+
+variable "security_group_ids" {
+  type = "list"
+}
+
+resource "aws_launch_template" "template" {
+  name = "${var.env}-${var.role}"
+
+  iam_instance_profile {
+    name = "${var.iam_instance_profile_name}"
+  }
+
+  image_id = "${lookup(var.ami_id_map, var.role, var.default_ami_id)}"
+
+  instance_initiated_shutdown_behavior = "${var.instance_initiated_shutdown_behavior}"
+
+  instance_type = "${var.instance_type}"
+
+  user_data = "${var.user_data}"
+
+  monitoring {
+    enabled = true
+  }
+
+  vpc_security_group_ids = ["${var.security_group_ids}"]
+
+  tag_specifications {
+    resource_type = "instance"
+    tags {
+      Name = "asg-${var.env}-${var.role}",
+      prefix = "${var.role}",
+      domain = "${var.env}.${var.root_domain}"
+      env = "${var.env}"
+    }
+  }
+
+  tag_specifications {
+    resource_type = "volume"
+    tags {
+      Name = "asg-${var.env}-${var.role}",
+      prefix = "${var.role}",
+      domain = "${var.env}.${var.root_domain}"
+      env = "${var.env}"
+    }
+  }
+}
+
+output "template_id" {
+  value = "${aws_launch_template.template.id}"
+}
+output "template_name" {
+  value = "${aws_launch_template.template.name}"
+}
