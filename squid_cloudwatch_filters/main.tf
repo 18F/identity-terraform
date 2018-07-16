6ecdb5de66323448ce45fcbd3f2f50ff33966b9a
Andy Brody <andy.brody@beta.dhs.gov>
2018-07-16 19:44:21 -0400

    Add cloudwatch logs filters for proxy requests.
    Create metrics for total request and denied requests, and add an alarm
    that fires to a specified SNS ARN on denials.

---
+++ squid_cloudwatch_filters/main.tf
@@ -0,0 +1,71 @@
+variable "env_name" {
+    description = "Environment name, for prefixing the generated metric names"
+}
+
+variable "log_group_name_override" {
+    default = ""
+    description = "The name of the log group, overriding the default <env_name>_/var/log/squid/access.log"
+}
+
+variable "metric_namespace" {
+    description = "Namespace to use for created cloudwatch metrics"
+    default = "LogMetrics/squid"
+}
+
+variable "treat_missing_data" {
+    description = "How to treat missing metric data in the denials alarm. https://www.terraform.io/docs/providers/aws/r/cloudwatch_metric_alarm.html#treat_missing_data"
+    default = "breaching"
+}
+
+variable "alarm_actions" {
+    type = "list"
+    description = "A list of ARNs to notify when the squid denied alarm fires"
+}
+
+locals {
+    log_group_name = "${var.log_group_name_override == "" ? "${var.env_name}_/var/log/squid/access.log" : var.log_group_name_override}"
+}
+
+
+resource "aws_cloudwatch_log_metric_filter" "squid_requests_total" {
+    name = "${var.env_name}-squid-requests-total"
+    pattern = "" # all events
+    log_group_name = "${local.log_group_name}"
+    metric_transformation {
+        namespace = "${var.metric_namespace}"
+        name = "${var.env_name}/TotalRequests"
+        value = 1
+        default_value = 0
+    }
+}
+
+resource "aws_cloudwatch_log_metric_filter" "squid_requests_denied" {
+    name = "${var.env_name}-squid-requests-denied"
+    pattern = "\"DENIED\"" # logs containing DENIED anywhere
+    log_group_name = "${local.log_group_name}"
+    metric_transformation {
+        namespace = "${var.metric_namespace}"
+        name = "${var.env_name}/DeniedRequests"
+        value = 1
+        default_value = 0
+    }
+}
+
+resource "aws_cloudwatch_metric_alarm" "squid_denied_alarm" {
+    alarm_name = "${var.env_name}-squid-denials"
+    alarm_description = "(Managed by Terraform) Alarm when the Squid access log shows any denied requests"
+    namespace = "${var.metric_namespace}"
+    metric_name = "${var.env_name}/DeniedRequests"
+
+    # alert when sum(denials) >= 1 for any 1 minute out of 15 eval periods
+    statistic = "Sum"
+    comparison_operator = "GreaterThanOrEqualToThreshold"
+    threshold = 1
+    period = 60
+    datapoints_to_alarm = 1
+    evaluation_periods = 15
+
+    treat_missing_data = "${var.treat_missing_data}"
+
+    alarm_actions = "${var.alarm_actions}"
+}
