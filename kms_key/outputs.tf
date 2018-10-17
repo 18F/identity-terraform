81e795f7368ab6c8b07ac239bb656db926c363a5
Brian Crissup <crissupb@amazon.com>
2018-10-17 11:39:11 -0400

    Fix issue with outputs

--- kms_key/outputs.tf
+++ kms_key/outputs.tf
@@ -2,7 +2,7 @@ output "kms_key_arn" {
     description = "KMS key arn"
     value = "${aws_kms_key.key.arn}"
 }
-
+ß
 output "kms_key_id" {
     description = "KMS key id"
     value = "${aws_kms_key.key.key_id}"
@@ -10,5 +10,5 @@ output "kms_key_id" {
 
 output "kms_key_alias" {
     description = "KMS key alias"
-    value = "${aws_kms_key.alias}"
+    value = "${aws_kms_key.key.alias}"
 }
