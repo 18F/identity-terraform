9b31231543e9974136defce40dbf66133fa40182
Brian Crissup <crissupb@amazon.com>
2018-10-17 14:41:55 -0400

    Update key alias

--- kms_key/outputs.tf
+++ kms_key/outputs.tf
@@ -2,13 +2,13 @@ output "kms_key_arn" {
     description = "KMS key arn"
     value = "${aws_kms_key.key.arn}"
 }
-ß
+
 output "kms_key_id" {
     description = "KMS key id"
     value = "${aws_kms_key.key.key_id}"
 }
 
-output "kms_key_alias" {
-    description = "KMS key alias"
-    value = "${aws_kms_key.key.alias}"
+output "kms_key_alias_arn" {
+    description = "KMS key alias arn"
+    value = "${aws_kms_alias.alias.arn}"
 }
