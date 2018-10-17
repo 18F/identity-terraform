08189dcbf15cc487c8ee47d36df0ccf64cf21ed3
Brian Crissup <crissupb@amazon.com>
2018-10-17 14:35:39 -0400

    Update

--- lambda/main.tf
+++ lambda/main.tf
@@ -5,7 +5,7 @@ resource "aws_lambda_function" "lambda" {
   description      = "${var.lambda_description}"
   memory_size      = "${var.lambda_memory}"
   timeout          = "${var.lambda_timeout}"
-  role             = "${aws_iam_role.lambda}"
+  role             = "${aws_iam_role.lambda.arn}"
   handler          = "${var.lambda_handler}"
   source_code_hash = "${base64sha256(file("${var.source_key}.zip"))}"
   runtime          = "${var.lambda_runtime}"
