2dc82332a105c47dd5695c53015bbb6b857c3016
Brian Crissup <crissupb@amazon.com>
2018-11-27 13:56:47 -0500

    Correct outputs

--- secret_without_rotation/outputs.tf
+++ secret_without_rotation/outputs.tf
@@ -1,9 +1,9 @@
 output "id" {
     description = "Secret Id"
-    value = "${aws_secretsmanager_secret.secret_with_rotation.id}"
+    value = "${aws_secretsmanager_secret.secret_without_rotation.id}"
 }
 
 output "arn" {
     description = "Secret arn"
-    value = "${aws_secretsmanager_secret.secret_with_rotation.arn}"
+    value = "${aws_secretsmanager_secret.secret_without_rotation.arn}"
 }
