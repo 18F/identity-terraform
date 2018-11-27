57f88f1d902167be291db4cc1a9eca69cdd47c64
Brian Crissup <crissupb@amazon.com>
2018-11-27 14:46:41 -0500

    Add variables for lambda

--- secret_with_rotation/variables.tf
+++ secret_with_rotation/variables.tf
@@ -32,3 +32,26 @@ variable "secret_rotation_days" {
     description = "Number of days between secret rotation"
     default = "15"
 }
+
+variable "password_rotation_lambda_source_key" {
+    description = "Path in S3 bucket to lambda function example: kinesis/events_transform.zip"
+}
+
+variable "password_rotation_lambda_memory" {
+    description = "Amount of RAM to assign to lambda function min 128 max 3008 in 64mb increments"
+    default = "128"
+}
+
+variable "password_rotation_lambda_timeout" {
+    description = "Lambda timeout"
+    default = "120"
+}
+
+variable "password_rotation_lambda_handler" {
+    description = "Handler function name"
+}
+
+variable "password_rotation_lambda_runtime" {
+    description = "Lambda runtime"
+    default = "python2.7"
+}
