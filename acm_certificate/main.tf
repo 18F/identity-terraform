bbab8df9ab1883397b612e0973bae200b5676b1e
Andy Brody <andy.brody@usds.dhs.gov>
2019-10-15 18:02:52 -0400

    Workaround for TF/AWS bug: Ignore changes in sANs.
    Terraform is perpetually recreating certain AWS ACM certificates because
    the AWS API has started returning the list of subjectAltNames in a
    nondeterministic order.
    
    Work around this for the time being by ignoring changes to the
    subjectAltNames. We will need to delete this line if we want to make
    changes to the subjectAltNames in the future.
    
    https://github.com/18F/identity-devops/issues/1469

--- acm_certificate/main.tf
+++ acm_certificate/main.tf
@@ -50,6 +50,13 @@ resource "aws_acm_certificate" "main" {
   lifecycle {
     create_before_destroy = true
   }
+
+  # TODO: this is a workaround for an AWS API / Terraform AWS provider bug
+  # https://github.com/terraform-providers/terraform-provider-aws/issues/8531
+  # https://github.com/18F/identity-devops/issues/1469
+  lifecycle {
+    ignore_changes = ["subject_alternative_names"]
+  }
 }
 
 # Create each validation CNAME
