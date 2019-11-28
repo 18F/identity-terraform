c1871cfd14f79f99ae844a4403cdb36fb659c06b
bleachbyte <jonathan.pirro@gsa.gov>
2019-11-27 16:16:35 -0800

    testing change made with 0.12upgrade to acm_certificate

--- acm_certificate/main.tf
+++ acm_certificate/main.tf
@@ -5,12 +5,12 @@ variable "domain_name" {
 }
 
 variable "enabled" {
-  default = 1
+  default     = 1
   description = "Like count, but for the whole module. 1 for True, 0 for False."
 }
 
 variable "subject_alternative_names" {
-  default = []
+  default     = []
   description = "A list of additional names to add to the certificate"
 }
 
@@ -26,17 +26,15 @@ variable "validation_cname_ttl" {
 
 output "cert_arn" {
   description = "ARN of the issued ACM certificate"
-  value = "${element(concat(aws_acm_certificate.main.*.arn, list("")), 0)}"
+  value       = element(concat(aws_acm_certificate.main.*.arn, [""]), 0)
 }
 
 output "finished_id" {
   description = "Reference this output in order to depend on validation being complete."
-  value = "${element(concat(aws_acm_certificate_validation.main.*.id, list("")), 0)}"
+  value       = element(concat(aws_acm_certificate_validation.main.*.id, [""]), 0)
 }
 
-
 # -- Resources --
-
 
 # Create the certificate with the specified SubjectAltNames
 resource "aws_acm_certificate" "main" {
@@ -45,7 +43,7 @@ resource "aws_acm_certificate" "main" {
   domain_name               = var.domain_name
   subject_alternative_names = var.subject_alternative_names
 
-  validation_method         = "DNS"
+  validation_method = "DNS"
 
   lifecycle {
     create_before_destroy = true
@@ -59,11 +57,19 @@ resource "aws_acm_certificate" "main" {
 
 # Create each validation CNAME
 resource "aws_route53_record" "validation-cnames" {
-  count   = (length(var.subject_alternative_names) + 1) * var.enabled
-  name    = aws_acm_certificate.main[0].domain_validation_options[count.index].resource_record_name
-  type    = aws_acm_certificate.main[0].domain_validation_options[count.index].resource_record_type
+  count   = length(var.subject_alternative_names) + 1 * var.enabled
+  name    = aws_acm_certificate.main.0.domain_validation_options[count.index]["resource_record_name"]
+  type    = aws_acm_certificate.main.0.domain_validation_options[count.index]["resource_record_type"]
   zone_id = var.validation_zone_id
-  records = [aws_acm_certificate.main[0].domain_validation_options[count.index].resource_record_value]
+  # TF-UPGRADE-TODO: In Terraform v0.10 and earlier, it was sometimes necessary to
+  # force an interpolation expression to be interpreted as a list by wrapping it
+  # in an extra set of list brackets. That form was supported for compatibility in
+  # v0.11, but is no longer supported in Terraform v0.12.
+  #
+  # If the expression in the following list itself returns a list, remove the
+  # brackets to avoid interpretation as a list of lists. If the expression
+  # returns a single list item then leave it as-is and remove this TODO comment.
+  records = [aws_acm_certificate.main.0.domain_validation_options[count.index]["resource_record_value"]]
   ttl     = var.validation_cname_ttl
 }
 
@@ -72,7 +78,8 @@ resource "aws_route53_record" "validation-cnames" {
 resource "aws_acm_certificate_validation" "main" {
   count = var.enabled
 
-  certificate_arn = aws_acm_certificate.main[count.index].arn
+  certificate_arn = aws_acm_certificate.main[0].arn
 
-  validation_record_fqdns = aws_route53_record.validation-cnames[*].fqdn
+  validation_record_fqdns = aws_route53_record.validation-cnames.*.fqdn
 }
+
