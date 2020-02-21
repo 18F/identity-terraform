99eb230e7ecf64838d4eef07f730bc552d15723a
bleachbyte <jonathan.pirro@gsa.gov>
2020-02-21 10:11:43 -0800

    testing out object lists to have multiple policies/docs

--- iam_assumerole/main.tf
+++ iam_assumerole/main.tf
@@ -16,12 +16,13 @@ variable "master_assumerole_policy" {
   description = "HEREDOC; Policy document to attach to the role allowing AssumeRole access from a master account."
 }
 
-variable "policy_description" {
-  description = "Description of policy including access provided."
-}
-
-variable "policy_name" {
-  description = "Name of the IAM policy for the role."
+variable "iam_policies" {
+  description = "Name/description/document properties for policy/policies."
+  type = list(object({
+    policy_name        = string
+    policy_description = string
+    policy_document    = list(any)
+  }))
 }
 
 variable "role_duration" {
@@ -44,10 +45,10 @@ variable "statement" {
 
 # create policy document; iterate through statements{} via dynamic
 data "aws_iam_policy_document" "iam_policy_doc" {
-  count = var.enabled ? 1 : 0
+  count = var.enabled ? 1 * length(var.iam_policies) : 0
 
   dynamic "statement" {
-    for_each = var.statement
+    for_each = var.iam_policies[count.index].policy_document
     content {
       sid       = statement.value.sid
       effect    = statement.value.effect
@@ -55,6 +56,14 @@ data "aws_iam_policy_document" "iam_policy_doc" {
       resources = statement.value.resources
     }
   }
+}
+
+resource "aws_iam_policy" "iam_role_policy" {
+  count = var.enabled ? 1 * length(var.iam_policies) : 0
+
+  name        = var.iam_policies[count.index].policy_name
+  description = var.iam_policies[count.index].policy_description
+  policy      = data.aws_iam_policy_document.iam_policy_doc[count.index].json
 }
 
 resource "aws_iam_role" "iam_assumable_role" {
@@ -66,19 +75,11 @@ resource "aws_iam_role" "iam_assumable_role" {
   max_session_duration = var.role_duration #seconds
 }
 
-resource "aws_iam_policy" "iam_role_policy" {
-  count = var.enabled ? 1 : 0
-
-  name        = var.policy_name
-  description = var.policy_description
-  policy      = data.aws_iam_policy_document.iam_policy_doc[0].json
-}
-
 resource "aws_iam_role_policy_attachment" "policy_attachment_main" {
-  count = var.enabled ? 1 : 0
+  count = var.enabled ? 1 * length(var.iam_policies) : 0
 
   role       = aws_iam_role.iam_assumable_role[0].name
-  policy_arn = aws_iam_policy.iam_role_policy[0].arn
+  policy_arn = aws_iam_policy.iam_role_policy[count.index].arn
 }
 
 resource "aws_iam_role_policy_attachment" "policy_attachment_custom" {
@@ -86,16 +87,4 @@ resource "aws_iam_role_policy_attachment" "policy_attachment_custom" {
 
   role       = aws_iam_role.iam_assumable_role[0].name
   policy_arn = element(var.custom_policy_arns, count.index)
-}
-
-# -- Outputs --
-
-output "iam_role_arn" {
-  description = "ARN of the created IAM role."
-  value = aws_iam_role.iam_assumable_role[0].arn
-}
-
-output "iam_policy_arn" {
-  description = "ARN of the created IAM policy."
-  value = aws_iam_policy.iam_role_policy[0].arn
 }
