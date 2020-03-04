9a9b5c1f1d2ed2b7b0f77053f9337923cb39df8c
Steven Harms <steven.harms@gsa.gov>
2020-03-04 11:13:27 -0700

    Switch to only aws_iam_policy

--- ssm/main.tf
+++ ssm/main.tf
@@ -49,36 +49,11 @@ data "aws_iam_policy_document" "ssm" {
   }
 }
 
-# policy to allow ssm access to ec2
-data "aws_iam_policy_document" "assume_role" {
-  statement {
-    sid     = "AssumeRole"
-    actions = ["sts:AssumeRole"]
-
-    principals {
-      type        = "Service"
-      identifiers = ["ec2.amazonaws.com"]
-    }
-  }
-}
-
 # -- Resources --
-resource "aws_iam_role" "ec2_ssm_instance_profile_role" {
-  count              = var.enabled
-  name               = "${var.env_name}_ec2_ssm_role"
-  path               = "/"
-  assume_role_policy = data.aws_iam_policy_document.assume_role.json
-}
-
-resource "aws_iam_role_policy" "ec2_ssm_instance_profile_policy" {
-  count              = var.enabled
-  name               = "${var.env_name}_ec2_ssm_policy"
-  role               = aws_iam_role.ec2_ssm_instance_profile_role[0].id
-  policy             = data.aws_iam_policy_document.ssm.json
-}
-
-resource "aws_iam_instance_profile" "ec2_ssm_instance_profile" {
-  count              = var.enabled
-  name               = "${var.env_name}_ec2_ssm_instance_profile"
-  role               = aws_iam_role.ec2_ssm_instance_profile_role[0].name
+resource "aws_iam_policy" "ec2_ssm_policy" {
+  count       = var.enabled
+  name        = "${var.env_name}_ec2_ssm_policy"
+  path        = "/"
+  description = "Allow SSM session management"
+  policy      = data.aws_iam_policy_document.ssm.json
 }
