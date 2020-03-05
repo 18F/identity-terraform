c991ae73d0dca6aa11af2a0dcda93e2c80dfbceb
Steven Harms <steven.harms@gsa.gov>
2020-03-04 17:01:01 -0700

    Add conditional for disabled

--- ssm/main.tf
+++ ssm/main.tf
@@ -60,5 +60,5 @@ resource "aws_iam_policy" "ec2_ssm_policy" {
 
 # -- Outputs --
 output "ssm_iam_policy_arn" {
-  value = aws_iam_policy.ec2_ssm_policy[0].arn
+  value = var.enabled ? aws_iam_policy.ec2_ssm_policy[0].arn : null
 }
