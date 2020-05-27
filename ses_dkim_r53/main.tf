5ae6fa08c71ff983ecce75ec52ccaa4697bfb143
bleachbyte <jonathan.pirro@gsa.gov>
2020-05-27 11:37:50 -0700

    removing unnecessary manual- variables as the resources can be imported into state

--- ses_dkim_r53/main.tf
+++ ses_dkim_r53/main.tf
@@ -12,18 +12,6 @@ variable "zone_id" {
   default     = "ABCDEFGHIJ123"
 }
 
-variable "manual_verification_token" {
-  description = "SES verification token, if previously created outside of Terraform"
-  type        = string
-  default     = ""
-}
-
-variable "manual_dkim_tokens" {
-  description = "SES DKIM tokens, if previously created outside of Terraform"
-  type        = list(any)
-  default     = []
-}
-
 variable "ttl_verification_record" {
   description = "TTL value for the SES verification TXT record."
   type        = string
@@ -51,14 +39,14 @@ resource "aws_route53_record" "primary_verification_record" {
   name    = "_amazonses.${var.domain}"
   type    = "TXT"
   ttl     = var.ttl_verification_record
-  records = ["${var.manual_verification_token != "" ? var.manual_verification_token : aws_ses_domain_identity.primary.verification_token}"]
+  records = ["${aws_ses_domain_identity.primary.verification_token}"]
 }
 
 resource "aws_route53_record" "primary_ses_dkim" {
   count   = 3
   zone_id = var.zone_id
-  name    = "${length(var.manual_dkim_tokens) > 0 ? var.manual_dkim_tokens[count.index] : element(aws_ses_domain_dkim.primary.dkim_tokens, count.index)}._domainkey.${var.domain}"
+  name    = "${element(aws_ses_domain_dkim.primary.dkim_tokens, count.index)}._domainkey.${var.domain}"
   type    = "CNAME"
   ttl     = var.ttl_dkim_records
-  records = ["${length(var.manual_dkim_tokens) > 0 ? var.manual_dkim_tokens[count.index] : element(aws_ses_domain_dkim.primary.dkim_tokens, count.index)}.dkim.amazonses.com"]
+  records = ["${element(aws_ses_domain_dkim.primary.dkim_tokens, count.index)}.dkim.amazonses.com"]
 }
