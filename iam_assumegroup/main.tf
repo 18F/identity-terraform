b5070739a820ca4f385e211e286e578f4d612223
bleachbyte <jonathan.pirro@gsa.gov>
2020-06-01 12:37:19 -0700

    redesign of modules for easier resource naming/tracking

--- iam_assumegroup/main.tf
+++ iam_assumegroup/main.tf
@@ -10,18 +10,15 @@ variable "group_role_map" {
 }
 
 locals {
-  group_account_map = [
-    for line in flatten([
-      for item, access in
-      {
-        for group, perms in var.group_role_map : group => flatten([
-          for perm in perms: flatten([
-            for pair in setproduct(keys(perm), flatten([values(perm)])): join("", [pair[1], "Assume", pair[0]])
-          ])
+  role_group_map = transpose(
+    {
+      for group, perms in var.group_role_map : group => flatten([
+        for perm in perms: flatten([
+          for pair in setproduct(keys(perm), flatten([values(perm)])): join("", [pair[1], "Assume", pair[0]])
         ])
-      }: formatlist("%s %s", item, access)
-    ]): split(" ", line)
-  ]
+      ])
+    }
+  )
 }
 
 # -- Resources --
@@ -32,9 +29,11 @@ resource "aws_iam_group" "iam_group" {
   name = each.key
 }
 
-resource "aws_iam_group_policy_attachment" "iam_group_policies" {
-  count = length(local.group_account_map)
+resource "aws_iam_policy_attachment" "group_policy" {
+  for_each = local.role_group_map
   
-  group = element(local.group_account_map[count.index],0)
-  policy_arn = "arn:aws:iam::${var.master_account_id}:policy/${element(local.group_account_map[count.index],1)}"
+  name       = each.key
+  groups     = each.value
+  policy_arn = "arn:aws:iam::${var.master_account_id}:policy/${each.key}"
 }
+
--- iam_masterusers/main.tf
+++ iam_masterusers/main.tf
@@ -5,6 +5,14 @@ variable "user_map" {
   type        = map(list(string))
 }
 
+variable "allow_self_management" {
+  description = <<EOM
+Whether or not to create the 'ManageYourAccount' IAM policy and
+attach it to all users in var.user_map in this account.
+EOM
+  type        = bool
+  default     = true
+}
 
 # -- Resources --
 
@@ -15,9 +23,172 @@ resource "aws_iam_user" "master_user" {
   force_destroy = true
 }
 
-resource "aws_iam_user_group_membership" "master_user" {
-  for_each = var.user_map
+resource "aws_iam_group_membership" "master_group" {
+  for_each = transpose(var.user_map)
 
-  user = each.key
-  groups = each.value
+  name = "${each.key}-group"
+  group = each.key
+  users = each.value
+}
+
+resource "aws_iam_policy" "manage_your_account" {
+  count = var.allow_self_management ? 1 : 0
+
+  name        = "ManageYourAccount"
+  path        = "/"
+  description = "Policy for account self management"
+  policy      = data.aws_iam_policy_document.manage_your_account.json
+}
+
+resource "aws_iam_policy_attachment" "manage_your_account" {
+  count = var.allow_self_management ? 1 : 0
+
+  name = "ManageYourAccount"
+  users = keys(var.user_map)
+  policy_arn = aws_iam_policy.manage_your_account[0].arn
+}
+
+data "aws_iam_policy_document" "manage_your_account" {
+  statement {
+    sid    = "AllowAllUsersToListAccounts"
+    effect = "Allow"
+    actions = [
+      "iam:ListAccountAliases",
+      "iam:ListUsers",
+      "iam:ListVirtualMFADevices",
+      "iam:GetAccountPasswordPolicy",
+      "iam:GetAccountSummary",
+    ]
+    resources = [
+      "*",
+    ]
+  }
+  statement {
+    sid    = "AllowAllUsersToListIAMResourcesWithMFA"
+    effect = "Allow"
+    actions = [
+      "iam:ListPolicies",
+      "iam:GetPolicyVersion",
+    ]
+    resources = [
+      "*",
+    ]
+    condition {
+      test     = "Bool"
+      variable = "aws:MultiFactorAuthPresent"
+      values = [
+        "true",
+      ]
+    }
+  }
+  statement {
+    sid    = "AllowIndividualUserToSeeAndManageOnlyTheirOwnAccountInformation"
+    effect = "Allow"
+    actions = [
+      "iam:ChangePassword",
+      "iam:CreateAccessKey",
+      "iam:CreateLoginProfile",
+      "iam:DeleteAccessKey",
+      "iam:DeleteLoginProfile",
+      "iam:GetLoginProfile",
+      "iam:GetUser",
+      "iam:GetAccessKeyLastUsed",
+      "iam:ListAccessKeys",
+      "iam:UpdateAccessKey",
+      "iam:UpdateLoginProfile",
+      "iam:ListSigningCertificates",
+      "iam:DeleteSigningCertificate",
+      "iam:UpdateSigningCertificate",
+      "iam:UploadSigningCertificate",
+      "iam:ListSSHPublicKeys",
+      "iam:GetSSHPublicKey",
+      "iam:DeleteSSHPublicKey",
+      "iam:UpdateSSHPublicKey",
+      "iam:UploadSSHPublicKey",
+      "iam:ListUserPolicies",
+      "iam:ListAttachedUserPolicies",
+      "iam:ListGroupsForUser",
+    ]
+    resources = [
+      "arn:aws:iam::*:user/$${aws:username}",
+    ]
+  }
+  statement {
+    sid    = "AllowIndividualUserToListOnlyTheirOwnMFA"
+    effect = "Allow"
+    actions = [
+      "iam:ListMFADevices",
+    ]
+    resources = [
+      "arn:aws:iam::*:mfa/*",
+      "arn:aws:iam::*:user/$${aws:username}",
+    ]
+  }
+  statement {
+    sid    = "AllowIndividualUserToManageTheirOwnMFA"
+    effect = "Allow"
+    actions = [
+      "iam:CreateVirtualMFADevice",
+      "iam:DeleteVirtualMFADevice",
+      "iam:EnableMFADevice",
+      "iam:ResyncMFADevice",
+    ]
+    resources = [
+      "arn:aws:iam::*:mfa/$${aws:username}",
+      "arn:aws:iam::*:user/$${aws:username}",
+    ]
+  }
+  statement {
+    sid    = "AllowIndividualUserToDeactivateOnlyTheirOwnMFAOnlyWhenUsingMFA"
+    effect = "Allow"
+    actions = [
+      "iam:DeactivateMFADevice",
+    ]
+    resources = [
+      "arn:aws:iam::*:mfa/$${aws:username}",
+      "arn:aws:iam::*:user/$${aws:username}",
+    ]
+    condition {
+      test     = "Bool"
+      variable = "aws:MultiFactorAuthPresent"
+      values = [
+        "true",
+      ]
+    }
+  }
+  statement {
+    sid    = "BlockMostAccessUnlessSignedInWithMFA"
+    effect = "Deny"
+    actions = [
+      "iam:DeleteVirtualMFADevice",
+      "iam:DeleteLoginProfile",
+      "iam:DeleteAccessKey",
+      "iam:DeactivateMFADevice",
+      "iam:ResyncMFADevice",
+      "iam:ListSSHPublicKeys",
+      "iam:DeleteSSHPublicKey",
+      "iam:UpdateSSHPublicKey",
+      "iam:UploadSSHPublicKey",
+      "iam:ListAccessKeys",
+      "iam:GetAccessKeyLastUsed",
+      "iam:ListServiceSpecificCredentials",
+      "iam:GetAccountSummary",
+      "iam:GetUser",
+      "iam:ListUserPolicies",
+      "iam:ListAttachedUserPolicies",
+      "iam:ListGroupsForUser",
+      "iam:GetPolicyVersion",
+      "sts:AssumeRole",
+    ]
+    resources = [
+      "*",
+    ]
+    condition {
+      test     = "Bool"
+      variable = "aws:MultiFactorAuthPresent"
+      values = [
+        "false",
+      ]
+    }
+  }
 }
