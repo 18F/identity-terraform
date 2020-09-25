36ecdc74c3436585568fab7abddb3336cec35d93
bleachbyte <jonathan.pirro@gsa.gov>
2020-09-25 16:06:33 -0500

    adding bucket_name_override to allow custom names outside of normal schema

--- s3_config/main.tf
+++ s3_config/main.tf
@@ -2,16 +2,25 @@
 variable "bucket_name_prefix" {
   description = "First substring in S3 bucket name of $bucket_name_prefix.$bucket_name.$account_id-$region"
   type        = string
+  default     = ""
 }
 
 variable "bucket_name" {
   description = "Main/second substring in S3 bucket name of $bucket_name_prefix.$bucket_name.$account_id-$region"
   type        = string
+  default = ""
+}
+
+variable "bucket_name_override" {
+  description = "Set this to override the normal bucket naming schema."
+  type        = string
+  default = ""
 }
 
 variable "region" {
-  default     = "us-west-2"
   description = "AWS Region"
+  type = string
+  default     = "us-west-2"
 }
 
 variable "inventory_bucket_arn" {
@@ -38,7 +47,13 @@ variable "optional_fields" {
 }
 
 locals {
-  bucket_fullname = "${var.bucket_name_prefix}.${var.bucket_name}.${data.aws_caller_identity.current.account_id}-${var.region}"
+  bucket_fullname = var.bucket_name_override != "" ? var.bucket_name_override : join(".",
+    [
+      var.bucket_name_prefix,
+      var.bucket_name,
+      "${data.aws_caller_identity.current.account_id}-${var.region}"
+    ]
+  )
 }
 
 # -- Data Sources --
