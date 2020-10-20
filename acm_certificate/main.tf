77d7376acc575dfa918d6964161daea7cd2a71ae
bleachbyte <jonathan.pirro@gsa.gov>
2020-10-20 12:01:48 -0500

    working version of acm_certificate module

--- acm_certificate/main.tf
+++ acm_certificate/main.tf
@@ -44,19 +44,26 @@ resource "aws_acm_certificate" "main" {
 
 # Create each validation CNAME
 resource "aws_route53_record" "validation-cnames" {
-  count = length(var.subject_alternative_names) + 1
+  for_each = {
+    for item in aws_acm_certificate.main.domain_validation_options: item.domain_name => {
+      name   = item.resource_record_name
+      record = item.resource_record_value
+      type   = item.resource_record_type
+    }
+  }
 
-  name    = aws_acm_certificate.main.domain_validation_options[count.index]["resource_record_name"]
-  type    = aws_acm_certificate.main.domain_validation_options[count.index]["resource_record_type"]
-  zone_id = var.validation_zone_id
-  records = [aws_acm_certificate.main.domain_validation_options[count.index]["resource_record_value"]]
-  ttl     = var.validation_cname_ttl
+  name            = each.value.name
+  type            = each.value.type
+  zone_id         = var.validation_zone_id
+  records         = [each.value.record]
+  ttl             = var.validation_cname_ttl
+  allow_overwrite = true
 }
 
 # Synthetic Terraform resource that blocks on validation completion
 # You can depend_on this to wait for the ACM cert to be ready.
 resource "aws_acm_certificate_validation" "main" {
   certificate_arn         = aws_acm_certificate.main.arn
-  validation_record_fqdns = aws_route53_record.validation-cnames.*.fqdn
+  validation_record_fqdns = [for record in aws_route53_record.validation-cnames: record.fqdn]
 }
 
