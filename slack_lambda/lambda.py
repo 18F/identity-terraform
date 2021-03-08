44228e8cad11197e88517fcad0c7530716955b7f
timothy-spencer <timothy.spencer@gsa.gov>
2021-03-08 14:37:39 -0800

    add formatting rule so that slack messages from codepipeline are actually readable

--- slack_lambda/lambda.py
+++ slack_lambda/lambda.py
@@ -4,10 +4,17 @@ import json
 http = urllib3.PoolManager()
 def lambda_handler(event, context):
     url = "${slack_webhook_url}"
+    eventmsg = event['Records'][0]['Sns']['Message']
+    try:
+      data = json.loads(eventmsg)
+      if data['detailType'] == 'CodePipeline Pipeline Execution State Change':
+        msgtext = 'auto-terraform:  ' + data['detail']['pipeline'] + ' pipeline ' + data['detail']['state'] + ' with execution ID ' + data['detail']['execution-id']
+    except:
+      msgtext = eventmsg
     msg = {
         "channel": "#${slack_channel}",
         "username": "${slack_username}",
-        "text": event['Records'][0]['Sns']['Message'],
+        "text": msgtext,
         "icon_emoji": "${slack_icon}"
     }
     
