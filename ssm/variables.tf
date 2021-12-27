3afe32d2e5aaa0f47cc6783a94cb50af815ac054
bleachbyte <jonathan.pirro@gsa.gov>
2021-12-27 16:06:23 -0600

    moving ssm module here as well

---
+++ ssm/variables.tf
@@ -0,0 +1,66 @@
+variable "ssm_doc_map" {
+  description = <<EOM
+REQUIRED. Map of data for SSM Documents. Each must include
+the document name, description, and command(s) to run at login.
+EOM
+  type        = map(map(string))
+  default = {
+    "default" = {
+      description = "Default login shell"
+      command     = "cd ; /bin/bash"
+    },
+  }
+}
+
+variable "region" {
+  description = "REQUIRED. AWS Region"
+  type        = string
+}
+
+variable "env_name" {
+  description = "REQUIRED. Environment name"
+  type        = string
+}
+
+variable "bucket_name_prefix" {
+  description = <<EOM
+REQUIRED. First substring in S3 bucket name of
+$bucket_name_prefix.$env_name-ssm-logs.$account_id-$region
+EOM
+  type        = string
+}
+
+variable "log_bucket_name" {
+  description = <<EOM
+(OPTIONAL) Override name of the bucket used for S3 logging.
+Will default to $bucket_name_prefix.s3-access-logs.$account_id-$region
+if not explicitly declared.
+EOM
+  type        = string
+  default     = ""
+}
+
+variable "inventory_bucket_name" {
+  description = <<EOM
+(OPTIONAL) Override name of the S3 bucket used for S3 Inventory reports.
+Will default to $bucket_name_prefix.s3-inventory.$account_id-$region
+if not explicitly declared.
+EOM
+  type        = string
+  default     = ""
+}
+
+locals {
+  bucket_name_suffix = "${data.aws_caller_identity.current.account_id}-${var.region}"
+  s3_bucket_name     = "${var.bucket_name_prefix}.${var.env_name}-ssm-logs.${local.bucket_name_suffix}"
+
+  log_bucket = var.log_bucket_name != "" ? var.log_bucket_name : join(".",
+    [var.bucket_name_prefix, "s3-access-logs", local.bucket_name_suffix]
+  )
+
+  inventory_bucket = var.inventory_bucket_name != "" ? var.inventory_bucket_name : join(".",
+    [var.bucket_name_prefix, "s3-inventory", local.bucket_name_suffix]
+  )
+
+  ssm_kms_keys = ["s3", "sessions"]
+}
