af12fae122fc172212cf1f55efbb50e44887062d
bleachbyte <jonathan.pirro@gsa.gov>
2022-03-08 22:58:45 -0600

    remove every-one-day event rule as it is account-wide

--- slo_lambda/variables.tf
+++ slo_lambda/variables.tf
@@ -1,13 +1,31 @@
-variable "name" {
-  description = "Unique name to use for resources"
+locals {
+  name = "${var.env_name}-cloudwatch-sli"
+}
+
+variable "env_name" {
+  description = "Environment name"
   type        = string
-  default     = "cloudwatch_sli"
+}
+
+variable "every_one_day_rule" {
+  description = <<EOM
+Name of a CloudWatch Event Rule with schedule_expression:rate(1 day) configured
+at the account (vs. environment) level.
+EOM
+  type        = string
+  default     = "every-one-day"
 }
 
 variable "lambda_runtime" {
   description = "Lambda runtime"
   type        = string
   default     = "python3.8"
+}
+
+variable "slo_lambda_code" {
+  type        = string
+  description = "Path of the compressed lambda source code."
+  default     = "src/windowed_slo.zip"
 }
 
 variable "window_days" {
