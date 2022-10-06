bb9cc5131ead5808ce5f7a92851a00c539cf61ef
Ryan Brown <ryan.d.brown@gsa.gov>
2022-10-06 13:27:19 -0700

    change elb s3 bucket encryption type to support NLBs

--- elb_access_logs_bucket/main.tf
+++ elb_access_logs_bucket/main.tf
@@ -56,7 +56,7 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "logs" {
 
   rule {
     apply_server_side_encryption_by_default {
-      sse_algorithm = "aws:kms"
+      sse_algorithm = "AES256"
     }
   }
 }
