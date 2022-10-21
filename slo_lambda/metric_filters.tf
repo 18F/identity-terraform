48ef60632915ca9800ca8e7e0357b4f847d9db92
Alex Kritikos <alex.kritikos@gsa.gov>
2022-10-21 15:16:04 -0500

    transformation

--- slo_lambda/metric_filters.tf
+++ slo_lambda/metric_filters.tf
@@ -1,21 +1,35 @@
 locals {
-    idp_uri_denylist_filter = join(" && ", formatlist("$.uri_path != \"%s\"", [
-            "/manifest.json",
-            "/",
-            "rules_of_use",
-            "/favicon-32x32.png",
-            "/health_check",
-            "/apple-touch-icon.png",
-            "/es"
-    ]))
+  idp_uri_denylist_filter = join(" && ", formatlist("$.uri_path != \"%s\"", [
+    "/manifest.json",
+    "/",
+    "rules_of_use",
+    "/favicon-32x32.png",
+    "/health_check",
+    "/apple-touch-icon.png",
+    "/es"
+  ]))
 }
 
 resource "aws_cloudwatch_log_metric_filter" "idp_filtered_uris_success" {
-    name = "${var.env_name}-idp-filtered-uris-success"
-    pattern = concat("{", local.idp_uri_denylist_filter, " && ($.status = 2* || $.status = 3*)}")
+  name           = "${var.env_name}-idp-filtered-uris-success"
+  log_group_name = "${var.env_name}_/var/log/nginx/access.log"
+  pattern        = concat("{", local.idp_uri_denylist_filter, " && ($.status = 2* || $.status = 3*)}")
+  metric_transformation {
+    name       = "FilteredUrisSuccess"
+    namespace  = "${var.env_name}/sli"
+    value      = 1
+    dimensions = {}
+  }
 }
 
 resource "aws_cloudwatch_log_metric_filter" "idp_filtered_uris_total" {
-    name = "${var.env_name}-idp-filtered-uris-success"
-    pattern = concat("{", local.idp_uri_denylist_filter, "}")
+  name    = "${var.env_name}-idp-filtered-uris-success"
+  log_group_name = "${var.env_name}_/var/log/nginx/access.log"
+  pattern = concat("{", local.idp_uri_denylist_filter, "}")
+  metric_transformation {
+    name       = "FilteredUrisTotal"
+    namespace  = "${var.env_name}/sli"
+    value      = 1
+    dimensions = {}
+  }
 }
