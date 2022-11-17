b3767d4842945f89e075a01c1210b0cc00d161c4
bleachbyte <jonathan.pirro@gsa.gov>
2022-11-17 16:10:15 -0600

    add increments in action names when using multiple schedules

--- asg_recycle/main.tf
+++ asg_recycle/main.tf
@@ -9,7 +9,10 @@ locals {
 resource "aws_autoscaling_schedule" "recycle_spinup" {
   for_each = toset(local.schedule["recycle_up"])
 
-  scheduled_action_name  = "auto-recycle.spinup"
+  scheduled_action_name = join(".", [
+    "auto-recycle.spinup",
+    index(local.schedule["recycle_up"], each.key)
+  ])
   min_size               = var.min_size
   max_size               = var.max_size
   desired_capacity       = var.normal_desired * var.spinup_mult_factor
@@ -21,9 +24,12 @@ resource "aws_autoscaling_schedule" "recycle_spinup" {
 resource "aws_autoscaling_schedule" "recycle_spindown" {
   for_each = toset(local.schedule["recycle_down"])
 
-  scheduled_action_name = "auto-recycle.spindown"
-  min_size              = var.min_size
-  max_size              = var.max_size
+  scheduled_action_name = join(".", [
+    "auto-recycle.spindown",
+    index(local.schedule["recycle_down"], each.key)
+  ])
+  min_size = var.min_size
+  max_size = var.max_size
   desired_capacity = var.override_spindown_capacity == -1 ? (
   var.normal_desired) : var.override_spindown_capacity
   recurrence             = each.key
@@ -39,8 +45,11 @@ resource "aws_autoscaling_schedule" "recycle_spindown" {
 resource "aws_autoscaling_schedule" "autozero_spinup" {
   for_each = toset(local.schedule["autozero_up"])
 
-  scheduled_action_name = "auto-zero.spinup"
-  min_size              = var.normal_min
+  scheduled_action_name = join(".", [
+    "auto-zero.spinup",
+    index(local.schedule["autozero_up"], each.key)
+  ])
+  min_size = var.normal_min
   max_size = var.normal_max == 0 ? (
   var.normal_min == 0 ? 1 : var.normal_min) : var.normal_max
   desired_capacity = (
@@ -55,7 +64,10 @@ resource "aws_autoscaling_schedule" "autozero_spinup" {
 resource "aws_autoscaling_schedule" "autozero_spindown" {
   for_each = toset(local.schedule["autozero_down"])
 
-  scheduled_action_name  = "auto-zero.spindown"
+  scheduled_action_name = join(".", [
+    "auto-zero.spindown",
+    index(local.schedule["autozero_down"], each.key)
+  ])
   min_size               = 0
   max_size               = var.max_size
   desired_capacity       = 0
