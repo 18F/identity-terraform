6197a0936946d1baee0b2ef3d73cb4cc9199317a
ryan.d.brown <ryan.d.brown@gsa.gov>
2023-01-03 13:30:13 -0800

    ouput latest version for asg auto instance refresh

--- launch_template/output.tf
+++ launch_template/output.tf
@@ -5,3 +5,7 @@ output "template_id" {
 output "template_name" {
   value = aws_launch_template.template.name
 }
+
+output "latest_version" {
+  value = aws_launch_template.template.latest_version
+}
