f83f7e0f567abcb42897c098f17059346b0f3c58
Alex Kritikos <alex.kritikos@gsa.gov>
2023-02-08 21:08:01 -0600

    make it work!

--- slo_lambda/src/windowed_slo_test.py
+++ slo_lambda/src/windowed_slo_test.py
@@ -11,7 +11,7 @@ os.environ['SLIS'] = ''
 
 # This import relies on our env var insertions above, so can't be reordered
 # autopep8: off
-from windowed_slo import parse_sli_json, publish_slis, Cloudwatch
+from windowed_slo import parse_sli_json, publish_slis, Cloudwatch, SLI_NAMESPACE, SLI_PREFIX
 # autopep8: on
 
 LOAD_BALANCER_ID = 'app/login-idp-alb-pretend/1234'
@@ -55,36 +55,57 @@ def test_simple_sli():
     with Stubber(cw) as stubber:
         stubber.add_response(
             *get_metric_statistics('HTTPCode_Target_2XX_Count', 2))
-        stubber.add_response(*get_metric_statistics('RequestCount', 4))
+        stubber.add_response(
+            *get_metric_statistics('RequestCount', 4))
         stubber.add_response(
             *get_metric_statistics('HTTPCode_ELB_5XX_Count', 2))
         stubber.add_response(
             *put_metric_data('test-http-200-availability', 1/3))
+        
         Cloudwatch.cloudwatch_client = cw
 
         sli_config = {
             "http-200-availability": {
-                'numerator': {
-                    'type': 'SingleMetric',
-                    'namespace': 'AWS/ApplicationELB',
-                    'metric_name': 'HTTPCode_Target_2XX_Count',
-                    'dimensions': [
-                        {
-                            'Name': 'LoadBalancer',
-                            'Value': load_balancer_id(LOAD_BALANCER_ARN),
-                        },
-                    ],
-                },
-                'denominator': {
-                    'type':  "LBValidRequestMetric",
-                    'load_balancer_arn': LOAD_BALANCER_ARN,
-                },
+                'numerator': [
+                    {
+                        'namespace': 'AWS/ApplicationELB',
+                        'metric_name': 'HTTPCode_Target_2XX_Count',
+                        'dimensions': [
+                            {
+                                'Name': 'LoadBalancer',
+                                'Value': LOAD_BALANCER_ID,
+                            },
+                        ],
+                    },
+                ],
+                'denominator': [
+                    {
+                        'namespace': 'AWS/ApplicationELB',
+                        'metric_name': 'RequestCount',
+                        'dimensions': [
+                            {
+                                'Name': 'LoadBalancer',
+                                'Value': LOAD_BALANCER_ID,
+                            },
+                        ],
+                    },
+                    {
+                        'namespace': 'AWS/ApplicationELB',
+                        'metric_name': 'HTTPCode_ELB_5XX_Count',
+                        'dimensions': [
+                            {
+                                'Name': 'LoadBalancer',
+                                'Value': LOAD_BALANCER_ID,
+                            },
+                        ],
+                    },
+                ]
             }
         }
 
         # The SLI config is assumed to be json, so convert the dict to json
         slis = parse_sli_json(json.dumps(sli_config))
-        create_slis(slis)
+        publish_slis(slis, SLI_NAMESPACE, SLI_PREFIX)
 
 
 def test_multiple_metric_sli():
@@ -97,26 +118,73 @@ def test_multiple_metric_sli():
             *get_metric_statistics('HTTPCode_Target_3XX_Count', 1))
         stubber.add_response(
             *get_metric_statistics('HTTPCode_Target_4XX_Count', 1))
-
-        stubber.add_response(*get_metric_statistics('RequestCount', 6))
+        stubber.add_response(
+            *get_metric_statistics('RequestCount', 6))
         stubber.add_response(
             *get_metric_statistics('HTTPCode_ELB_5XX_Count', 2))
-        stubber.add_response(*put_metric_data('test-all-availability', 0.5))
+        stubber.add_response(
+            *put_metric_data('test-all-availability', 0.5))
 
         Cloudwatch.cloudwatch_client = cw
 
         sli_config = {
             'all-availability': {
-                'numerator': {
-                    'type':  "LBGoodResponseMetric",
-                    'load_balancer_arn': LOAD_BALANCER_ARN,
-                },
-                'denominator': {
-                    'type':  "LBValidRequestMetric",
-                    'load_balancer_arn': LOAD_BALANCER_ARN,
-                },
+                'numerator': [
+                    {
+                        'namespace': 'AWS/ApplicationELB',
+                        'metric_name': 'HTTPCode_Target_2XX_Count',
+                        'dimensions': [
+                            {
+                                'Name': 'LoadBalancer',
+                                'Value': LOAD_BALANCER_ID,
+                            },
+                        ],
+                    },
+                    {
+                        'namespace': 'AWS/ApplicationELB',
+                        'metric_name': 'HTTPCode_Target_3XX_Count',
+                        'dimensions': [
+                            {
+                                'Name': 'LoadBalancer',
+                                'Value': LOAD_BALANCER_ID,
+                            },
+                        ],
+                    },
+                    {
+                        'namespace': 'AWS/ApplicationELB',
+                        'metric_name': 'HTTPCode_Target_4XX_Count',
+                        'dimensions': [
+                            {
+                                'Name': 'LoadBalancer',
+                                'Value': LOAD_BALANCER_ID,
+                            },
+                        ],
+                    },
+                ],
+                'denominator': [
+                                    {
+                        'namespace': 'AWS/ApplicationELB',
+                        'metric_name': 'RequestCountRequestCount',
+                        'dimensions': [
+                            {
+                                'Name': 'LoadBalancer',
+                                'Value': LOAD_BALANCER_ID,
+                            },
+                        ],
+                    },
+                    {
+                        'namespace': 'AWS/ApplicationELB',
+                        'metric_name': 'HTTPCode_ELB_5XX_Count',
+                        'dimensions': [
+                            {
+                                'Name': 'LoadBalancer',
+                                'Value': LOAD_BALANCER_ID,
+                            },
+                        ],
+                    },
+                ]
             }
         }
         # The SLI config is assumed to be json, so convert the dict to json
         slis = parse_sli_json(json.dumps(sli_config))
-        create_slis(slis)
+        publish_slis(slis, SLI_NAMESPACE, SLI_PREFIX)
