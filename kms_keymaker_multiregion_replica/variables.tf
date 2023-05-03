588c62066f17d3258d7629dffeee41e1d3bd49cc
Ryan Brown <ryan.d.brown@gsa.gov>
2023-05-03 10:55:00 -0700

    add comments

--- kms_keymaker_multiregion_replica/variables.tf
+++ kms_keymaker_multiregion_replica/variables.tf
@@ -14,10 +14,10 @@ variable "sqs_queue_arn" {
   description = "ARN of the SQS queue used as the CloudWatch event target."
 }
 
-variable "alarm_sns_topic_arn" {
-  type        = string
-  description = "ARN of the SNS topic used to send KMS alarms."
-}
+# variable "alarm_sns_topic_arn" {
+#   type        = string
+#   description = "ARN of the SNS topic used to send KMS alarms."
+# }
 
 variable "primary_key_arn" {
   type        = string
