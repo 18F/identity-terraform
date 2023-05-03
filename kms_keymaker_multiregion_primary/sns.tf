8122f7788d67d06539576cae2c2dd16832d53041
Ryan Brown <86372754+ryan-brown-sb@users.noreply.github.com>
2023-05-03 10:44:17 -0700

    Update kms_keymaker_multiregion_primary/sns.tf
    Co-authored-by: Jonathan Pirro <jonathan.pirro@gsa.gov>

--- kms_keymaker_multiregion_primary/sns.tf
+++ kms_keymaker_multiregion_primary/sns.tf
@@ -52,7 +52,7 @@ resource "aws_cloudwatch_event_target" "decrypt" {
   arn       = aws_sns_topic.kms_events.arn
 }
 
-#Send Cloudwatch Event to Alarm SNS Topic
+# send Cloudwatch events to alarm SNS topic
 resource "aws_cloudwatch_event_target" "replicate" {
   rule      = aws_cloudwatch_event_rule.replicate.name
   target_id = "${var.env_name}-sns"
