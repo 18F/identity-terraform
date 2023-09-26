245eaf63d13ebce38850cf4b6489bf0256df9d14
henry.richardson <henry.richardson@gsa.gov>
2023-09-26 17:14:02 +0000

    SQS Alerts Module

---
+++ sqs_alerts/main.tf
@@ -0,0 +1,76 @@
+locals {
+  #Inflight message limits are determined by queue type. Options are standard or FIFO
+  inflight_messages_limit = var.queue_type == "standard" ? 120000 : 20000
+}
+
+resource "aws_cloudwatch_metric_alarm" "inflight_messages" {
+  count = var.enabled ? 1 : 0
+
+  alarm_name        = "SQSInflightMessages_${var.queue_name}"
+  alarm_description = "SQS Inflight Messages has exceeded ${var.inflight_threshold}%"
+
+  comparison_operator       = "GreaterThanOrEqualToThreshold"
+  evaluation_periods        = var.evaluation_periods
+  threshold                 = local.inflight_messages_limit * (var.inflight_threshold * 0.01)
+  insufficient_data_actions = []
+
+  metric_name = "ApproximateNumberOfMessagesNotVisible"
+  namespace   = "AWS/SQS"
+  period      = var.period
+  statistic   = "Maximum"
+  dimensions = {
+    QueueName = var.queue_name
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_cloudwatch_metric_alarm" "age_of_oldest_message" {
+  count = var.enabled ? 1 : 0
+
+  alarm_name        = "SQSAgeOfOldestMessage_${var.queue_name}"
+  alarm_description = "SQS Age of Messages in the queue"
+
+  comparison_operator       = "GreaterThanOrEqualToThreshold"
+  evaluation_periods        = var.evaluation_periods
+  threshold                 = var.age_of_oldest_message_threshold
+  insufficient_data_actions = []
+
+  metric_name = "ApproximateAgeOfOldestMessage"
+  namespace   = "AWS/SQS"
+  period      = var.period
+  statistic   = "Maximum"
+  dimensions = {
+    QueueName = var.queue_name
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_cloudwatch_metric_alarm" "message_size" {
+  count = var.enabled ? 1 : 0
+
+  alarm_name        = "SQSMessageSize_${var.queue_name}"
+  alarm_description = "SQS Messages sizes have exceeded ${var.message_size_threshold}%"
+
+  comparison_operator       = "GreaterThanOrEqualToThreshold"
+  evaluation_periods        = var.evaluation_periods
+  threshold                 = var.max_message_size * (var.message_size_threshold * 0.01)
+  insufficient_data_actions = []
+
+  metric_name = "SentMessageSize"
+  namespace   = "AWS/SQS"
+  period      = var.period
+  statistic   = "Maximum"
+  dimensions = {
+    QueueName = var.queue_name
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
---
+++ sqs_alerts/variables.tf
@@ -0,0 +1,55 @@
+variable "enabled" {
+  type        = bool
+  default     = true
+  description = "Enables the set of alerts defined in this module"
+}
+
+variable "queue_name" {
+  type        = string
+  description = "The name of the SQS queue to monitor"
+}
+
+variable "queue_type" {
+  type        = string
+  default     = "standard"
+  description = "The type of SQS queue to monitor"
+
+  validation {
+    condition     = var.queue_type == "standard" || var.queue_type == "fifo"
+    error_message = "The queue type value must be \"standard\" or \"fifo\""
+  }
+}
+
+variable "inflight_threshold" {
+  type        = number
+  description = "The percentile threshold of inflight messages"
+  default     = 80
+}
+
+variable "max_message_size" {
+  type        = number
+  description = "The maximum message size supported by the queue"
+}
+
+variable "message_size_threshold" {
+  type        = number
+  description = "The percentile threshold of message sizes"
+  default     = 80
+}
+
+variable "age_of_oldest_message_threshold" {
+  type        = number
+  description = "The threshold for age_of_oldest_message. Defined in seconds."
+  default     = 300 # 5 Minutes
+}
+
+variable "evaluation_periods" {
+  type    = number
+  default = 1
+}
+
+variable "period" {
+  type        = number
+  description = "The period in seconds over which the specified statistic is applied"
+  default     = 60
+}
