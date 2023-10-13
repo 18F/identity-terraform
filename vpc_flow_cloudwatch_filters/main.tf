6124f076d6546d4438322cc9eb8ca13771914922
henry.richardson <henry.richardson@gsa.gov>
2023-10-13 14:49:49 +0000

    Typo

--- vpc_flow_cloudwatch_filters/main.tf
+++ vpc_flow_cloudwatch_filters/main.tf
@@ -24,7 +24,7 @@ variable "alarm_actions" {
 
 variable "default_field_list" {
   type        = list(any)
-  description = "Default filds for flow logs"
+  description = "Default fields for flow logs"
   default = [
     "version",
     "accountId",
