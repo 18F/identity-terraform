235ec27da14d1f65be002e33646c462c7f36c7a9
Mike Ward <michael.w.ward@gsa.gov>
2024-03-14 11:18:26 -0400

    Add new alarm, slack handles for notifications

--- squid_cloudwatch_filters/main.tf
+++ squid_cloudwatch_filters/main.tf
@@ -1,32 +1,3 @@
-variable "env_name" {
-  description = "Environment name, for prefixing the generated metric names"
-}
-
-variable "log_group_name_override" {
-  default     = ""
-  description = "The name of the log group, overriding the default <env_name>_/var/log/squid/access.log"
-}
-
-variable "metric_namespace" {
-  description = "Namespace to use for created cloudwatch metrics"
-  default     = "LogMetrics/squid"
-}
-
-variable "treat_missing_data" {
-  description = "How to treat missing metric data in the denials alarm."
-  default     = "notBreaching"
-}
-
-variable "alarm_actions" {
-  type        = list(string)
-  description = "A list of ARNs to notify when the squid denied alarm fires"
-}
-
-variable "runbook_url" {
-  type        = string
-  description = "A URL to a runbook to help triage alerts"
-}
-
 locals {
   log_group_name = var.log_group_name_override == "" ? "${var.env_name}_/var/log/squid/access.log" : var.log_group_name_override
 }
@@ -58,7 +29,7 @@ resource "aws_cloudwatch_log_metric_filter" "squid_requests_denied" {
 resource "aws_cloudwatch_metric_alarm" "squid_denied_alarm" {
   alarm_name        = "${var.env_name}-squid-denials"
   alarm_description = <<EOM
-The outbound proxy has denied too many outbound requests.
+The outbound proxy has denied outbound requests.
 
 Runbook: ${var.runbook_url}
 EOM
@@ -70,6 +41,32 @@ EOM
   statistic           = "Sum"
   comparison_operator = "GreaterThanOrEqualToThreshold"
   threshold           = 1
+  period              = 60
+  datapoints_to_alarm = 1
+  evaluation_periods  = 15
+
+  treat_missing_data = var.treat_missing_data
+
+  alarm_actions = var.alarm_actions
+}
+
+resource "aws_cloudwatch_metric_alarm" "squid_denied_alarm_critical" {
+  alarm_name        = "${var.env_name}-squid-denials-critical"
+  alarm_description = <<EOM
+The outbound proxy has denied too many outbound requests.
+
+Runbook: ${var.runbook_url}
+
+Notifying: ${join(", ", var.slack_handles)}
+EOM
+
+  namespace   = var.metric_namespace
+  metric_name = "${var.env_name}/DeniedRequests"
+
+  # alert when sum(denials) >= 25 for any 1 minute out of 15 eval periods
+  statistic           = "Sum"
+  comparison_operator = "GreaterThanOrEqualToThreshold"
+  threshold           = 25
   period              = 60
   datapoints_to_alarm = 1
   evaluation_periods  = 15
---
+++ squid_cloudwatch_filters/variables.tf
@@ -0,0 +1,33 @@
+variable "env_name" {
+  description = "Environment name, for prefixing the generated metric names"
+}
+
+variable "log_group_name_override" {
+  default     = ""
+  description = "The name of the log group, overriding the default <env_name>_/var/log/squid/access.log"
+}
+
+variable "metric_namespace" {
+  description = "Namespace to use for created cloudwatch metrics"
+  default     = "LogMetrics/squid"
+}
+
+variable "treat_missing_data" {
+  description = "How to treat missing metric data in the denials alarm."
+  default     = "notBreaching"
+}
+
+variable "alarm_actions" {
+  type        = list(string)
+  description = "A list of ARNs to notify when the squid denied alarm fires"
+}
+
+variable "runbook_url" {
+  type        = string
+  description = "A URL to a runbook to help triage alerts"
+}
+
+variable "slack_handles" {
+  type        = list
+  description = "A list of Slack group handles to tag in the alarm description"
+}
