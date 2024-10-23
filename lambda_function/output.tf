4aeddc5892df73f6b162ee7fdd010b32864f6ba9
henry.richardson <henry.richardson@gsa.gov>
2024-10-23 19:34:07 +0000

    Lambda Function: Additional Outputs

--- lambda_function/output.tf
+++ lambda_function/output.tf
@@ -3,9 +3,17 @@ output "function_name" {
 }
 
 output "lambda_arn" {
-  value = aws_lambda_function.lambda.arn
+  value       = aws_lambda_function.lambda.arn
+  description = "The ARN of the Lambda Function"
 }
 
 output "lambda_role_name" {
-  value = aws_iam_role.lambda.name
+  value       = aws_iam_role.lambda.name
+  description = "The name of the IAM Role associated with the lambda"
 }
+
+output "log_group_name" {
+  value       = aws_cloudwatch_log_group.lambda.name
+  description = "The name of the cloudwatch log group associated with the lambda"
+}
+
