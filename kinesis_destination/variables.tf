48744680090f3182779472916d8ea5753f5c7655
bleachbyte <jonathan.pirro@gsa.gov>
2024-11-14 16:39:59 -0600

    condense almost-duplicate kinesis_*_destination modules into kinesis_destination module

--- kinesis_destination/variables.tf
+++ kinesis_destination/variables.tf
@@ -1,25 +1,26 @@
 # Locals
 
 locals {
+  region = var.kinesis_region == "" ? data.aws_region.current.name : var.kinesis_region
   identifier_name = var.role_name == "" ? (
   "cloudwatch-to-kinesis-${local.region}") : var.role_name
   dest_acct_id  = data.aws_caller_identity.current.account_id
-  region        = data.aws_region.current.name
-  stream_region = var.stream_region == "" ? local.region : var.stream_region
 }
 
 # Variables
 
-variable "stream_arn" {
-  type        = string
-  description = "ARN of the Kinesis Data Stream that the Destination points to."
-}
-
-variable "stream_region" {
+variable "kinesis_arn" {
   type        = string
   description = <<EOM
-Region where the Kinesis Data Stream is located. If not specified,
-defaults to data.aws_region.current.name instead.
+ARN of the Kinesis resource (Firehose/Data Stream) that the Destination points to.
+EOM
+}
+
+variable "kinesis_region" {
+  type        = string
+  description = <<EOM
+Region where the Kinesis resource (Firehose/Data Stream) is located.
+If not specified, defaults to data.aws_region.current.name instead.
 EOM
   default     = ""
 }
@@ -33,7 +34,7 @@ variable "role_name" {
   type        = string
   description = <<EOM
 Identifier string used to name the IAM role/policies used by
-CloudWatch Logs for accessing Kinesis Data Stream/CloudWatch Destinations,
+CloudWatch Logs for accessing the Kinesis resource/CloudWatch Destinations,
 and the name of the CloudWatch Destination itself.
 Will default to "cloudwatch-to-kinesis-`local.region`" if not set.
 EOM
