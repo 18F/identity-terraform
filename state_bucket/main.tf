27a8091481f85dab05315e939846090dc685df7c
Henry Richardson <henry.richardson@gsa.gov>
2024-12-05 12:51:34 -0500

    Adding count reference

--- state_bucket/main.tf
+++ state_bucket/main.tf
@@ -226,9 +226,9 @@ resource "aws_appautoscaling_policy" "tf_lock_table_read_policy" {
 
   name               = "DynamoDBReadCapacityUtilization:${aws_appautoscaling_target.tf_lock_table_read_target[count.index].resource_id}"
   policy_type        = "TargetTrackingScaling"
-  resource_id        = aws_appautoscaling_target.tf_lock_table_read_target.resource_id
-  scalable_dimension = aws_appautoscaling_target.tf_lock_table_read_target.scalable_dimension
-  service_namespace  = aws_appautoscaling_target.tf_lock_table_read_target.service_namespace
+  resource_id        = aws_appautoscaling_target.tf_lock_table_read_target[count.index].resource_id
+  scalable_dimension = aws_appautoscaling_target.tf_lock_table_read_target[count.index].scalable_dimension
+  service_namespace  = aws_appautoscaling_target.tf_lock_table_read_target[count.index].service_namespace
 
   target_tracking_scaling_policy_configuration {
     predefined_metric_specification {
