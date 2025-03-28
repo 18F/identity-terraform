cea57dfeaa2e437852ffa488606bf37f954dce12
henry.richardson <henry.richardson@gsa.gov>
2025-03-28 14:22:09 +0000

    AssumeRole using aws_iam_policy_documents

--- iam_assumerole/main.tf
+++ iam_assumerole/main.tf
@@ -18,21 +18,7 @@ variable "master_assumerole_policy" {
 
 variable "iam_policies" {
   description = "Name/description/document properties for policy/policies."
-  type = list(object({
-    policy_name        = string
-    policy_description = string
-    policy_document = list(object({
-      sid       = string
-      effect    = string
-      actions   = list(string)
-      resources = list(string)
-      conditions = optional(list(object({
-        test     = string
-        variable = string
-        values   = list(string)
-      })))
-    }))
-  }))
+  type        = list(list(string))
 }
 
 variable "role_duration" {
@@ -76,30 +62,10 @@ EOM
 
 # -- Data Sources --
 
-# create policy document; iterate through statements{} via dynamic
+# create policy document
 data "aws_iam_policy_document" "iam_policy_doc" {
-  count = var.enabled ? 1 * length(var.iam_policies) : 0
-
-  dynamic "statement" {
-    for_each = var.iam_policies[count.index].policy_document
-    content {
-      sid       = statement.value.sid
-      effect    = statement.value.effect
-      actions   = statement.value.actions
-      resources = statement.value.resources
-
-      dynamic "condition" {
-        for_each = (
-          statement.value["conditions"] == null ? [] : statement.value["conditions"]
-        )
-        content {
-          test     = condition.value.test
-          variable = condition.value.variable
-          values   = condition.value.values
-        }
-      }
-    }
-  }
+  count                   = var.enabled ? length(var.iam_policies) : 0
+  source_policy_documents = var.iam_policies[count.index]
 }
 
 # obtain data/ARNs for every entry in var.custom_iam_policies
@@ -113,8 +79,8 @@ data "aws_iam_policy" "custom" {
 resource "aws_iam_policy" "iam_role_policy" {
   count = var.enabled ? 1 * length(var.iam_policies) : 0
 
-  name        = var.iam_policies[count.index].policy_name
-  description = var.iam_policies[count.index].policy_description
+  name        = "${var.role_name}${count.index + 1}"
+  description = "Policy ${count.index + 1} for ${var.role_name} role"
   policy      = data.aws_iam_policy_document.iam_policy_doc[count.index].json
 
   lifecycle {
@@ -123,7 +89,7 @@ resource "aws_iam_policy" "iam_role_policy" {
       # aws_iam_policy_document.json returns non-minified json structures but aws_iam_policy consumes the input as a minified json structure.
       # The jsondecode and jsonencode operations result in the minified json strucutre.
       condition     = length(jsonencode(jsondecode(data.aws_iam_policy_document.iam_policy_doc[count.index].json))) <= 6144
-      error_message = "The IAM policy exceeds the maximum allowed length (6144 characters.) Current Length: ${length(jsonencode(jsondecode(data.aws_iam_policy_document.iam_policy_doc[count.index].json)))}"
+      error_message = "The IAM policy ${var.role_name}${count.index + 1} exceeds the maximum allowed length (6144 characters.) Current Length: ${length(jsonencode(jsondecode(data.aws_iam_policy_document.iam_policy_doc[count.index].json)))}"
     }
   }
 }
