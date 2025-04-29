b5fbf652f04347ab80179cdd0a2e716b46389bb7
Mike Ward <michael.w.ward@gsa.gov>
2025-04-29 15:39:28 -0400

    Remove defaults

--- lambda_layer/variables.tf
+++ lambda_layer/variables.tf
@@ -1,22 +1,19 @@
 variable "source_code_filename" {
-  description = "(REQUIRED) Name (with extension) of file containing function source code."
+  description = "Name (with extension) of file containing function source code."
   type        = string
-  default     = "lambda_function.py"
 }
 
 variable "source_dir" {
   description = <<EOM
-(REQUIRED) Name of directory where source_code_filename + any other
+Name of directory where source_code_filename + any other
 files to be added to the ZIP file reside.
 EOM
   type        = string
-  default     = "src"
 }
 
 variable "zip_filename" {
-  description = "(REQUIRED) Desired name (with .zip extension) of resultant output file."
+  description = "Desired name (with .zip extension) of resultant output file."
   type        = string
-  default     = ""
 }
 
 variable "compatible_runtimes" {
