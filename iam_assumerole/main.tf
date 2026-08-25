491dfe2f94600f08557def7edd18a9ce2090a776
jonathan.pirro <jonathan.pirro@gsa.gov>
2026-08-25 17:22:51 +0000

    feat!(iam_assumerole): refactor to remove enabled var + add aws_iam_role_policy_attachments_exclusive resource (BREAKING)

--- iam_assumerole/main.tf
+++ iam_assumerole/main.tf
@@ -1,110 +1,39 @@
-# -- Variables --
+data "aws_iam_policy_document" "main" {
+  count = length(var.iam_policies)
 
-variable "enabled" {
-  description = "Whether or not to create the role/policy/attachment resources."
-  type        = bool
-  default     = true
-}
-
-variable "custom_iam_policies" {
-  description = "The names of any additional IAM policies to attach to the role."
-  type        = list(string)
-  default     = []
-}
-
-variable "master_assumerole_policy" {
-  description = "Policy document to attach to the role allowing AssumeRole access from a master account."
-}
-
-variable "iam_policies" {
-  description = "Name/description/document properties for policy/policies."
-  type        = list(list(string))
-}
-
-variable "role_duration" {
-  description = "Value for the max_session_duration for the role, in seconds."
-  type        = number
-  default     = 43200
-}
-
-variable "role_name" {
-  description = "Name of the IAM role."
-}
-
-variable "role_tags" {
-  description = "Tags to apply to the IAM role"
-  type        = map(string)
-  default = {
-    # RedshiftDbGroups  = "lg_users"    
-  }
-}
-
-variable "role_description" {
-  description = "A description/summary of the IAM role being created."
-  default     = ""
-}
-
-variable "permissions_boundary_policy_arn" {
-  description = <<EOM
-(REQUIRED) ARN of an existing IAM policy (from another module/source)
-which will be used as the Permissions Boundary for the IAM role.
-EOM
-  type        = string
-  default     = ""
-
-  ### OPTIONAL: To enforce that a Permissions Boundary policy must exist, and must
-  ### be attached to role(s) using this module, comment out the 'default' line/value
-  ### above, and uncomment the 'validation' block below. This will REQUIRE a valid
-  ### ARN from an existing Permissions Boundary policy created outside of the module.
-  #  validation {
-  #    condition = can(regex(
-  #      "^arn:aws:iam::[\\d]{12}:policy\\/[\\w+=,.@-]+$",
-  #      var.permissions_boundary_policy_arn
-  #    ))
-  #    error_message = <<EOM
-  #The permissions_boundary_policy_arn variable must be a valid AWS ARN,
-  #e.g.: arn:aws:iam::123456789012:policy/XCompanyBoundaries
-  #EOM
-  #  }
-}
-
-# -- Data Sources --
-
-# create policy document
-data "aws_iam_policy_document" "iam_policy_doc" {
-  count                   = var.enabled ? length(var.iam_policies) : 0
   source_policy_documents = var.iam_policies[count.index]
 }
 
-# obtain data/ARNs for every entry in var.custom_iam_policies
 data "aws_iam_policy" "custom" {
-  for_each = var.enabled ? toset(var.custom_iam_policies) : []
-  name     = each.key
+  for_each = toset(var.custom_iam_policies)
+
+  name = each.key
 }
 
-# -- Resources --
-
-resource "aws_iam_policy" "iam_role_policy" {
-  count = var.enabled ? 1 * length(var.iam_policies) : 0
+resource "aws_iam_policy" "main" {
+  count = length(var.iam_policies)
 
   name        = "${var.role_name}${count.index + 1}"
   description = "Policy ${count.index + 1} for ${var.role_name} role"
-  policy      = data.aws_iam_policy_document.iam_policy_doc[count.index].json
+  policy      = data.aws_iam_policy_document.main[count.index].json
 
+  # This precondition check validates IAM policies meet maximum length requirements and fails fast (in terraform plan
+  # operations). aws_iam_policy_document.json returns non-minified json, but aws_iam_policy consumes the input as a
+  # minified json structure. The jsondecode() and jsonencode() operations result in the minified json strucutre.
   lifecycle {
     precondition {
-      # This precondition check validates IAM policies meet maximum length requirements and fails fast (in terraform plan operations)
-      # aws_iam_policy_document.json returns non-minified json structures but aws_iam_policy consumes the input as a minified json structure.
-      # The jsondecode and jsonencode operations result in the minified json strucutre.
-      condition     = length(jsonencode(jsondecode(data.aws_iam_policy_document.iam_policy_doc[count.index].json))) <= 6144
-      error_message = "The IAM policy ${var.role_name}${count.index + 1} exceeds the maximum allowed length (6144 characters.) Current Length: ${length(jsonencode(jsondecode(data.aws_iam_policy_document.iam_policy_doc[count.index].json)))}"
+      condition = length(
+        jsonencode(jsondecode(data.aws_iam_policy_document.main[count.index].json))
+      ) <= 6144
+      error_message = <<EOM
+The IAM policy ${var.role_name}${count.index + 1} exceeds the maximum allowed length (6144 characters.)
+Current Length: ${length(jsonencode(jsondecode(data.aws_iam_policy_document.main[count.index].json)))}
+EOM
     }
   }
 }
 
-resource "aws_iam_role" "iam_assumable_role" {
-  count = var.enabled ? 1 : 0
-
+resource "aws_iam_role" "assumable" {
   name                 = var.role_name
   description          = var.role_description
   tags                 = var.role_tags
@@ -114,22 +43,54 @@ resource "aws_iam_role" "iam_assumable_role" {
   permissions_boundary = var.permissions_boundary_policy_arn
 }
 
-resource "aws_iam_role_policy_attachment" "policy_attachment_main" {
-  count = var.enabled ? 1 * length(var.iam_policies) : 0
+resource "aws_iam_role_policy_attachment" "main" {
+  count = length(var.iam_policies)
 
-  role       = aws_iam_role.iam_assumable_role[0].name
-  policy_arn = aws_iam_policy.iam_role_policy[count.index].arn
+  role       = aws_iam_role.assumable.name
+  policy_arn = aws_iam_policy.main[count.index].arn
 }
 
-resource "aws_iam_role_policy_attachment" "policy_attachment_custom" {
-  for_each = var.enabled ? data.aws_iam_policy.custom : {}
+resource "aws_iam_role_policy_attachment" "custom" {
+  for_each = data.aws_iam_policy.custom
 
-  role       = aws_iam_role.iam_assumable_role[0].name
+  role       = aws_iam_role.assumable.name
   policy_arn = each.value.arn
 }
 
-# -- Outputs --
+resource "aws_iam_role_policy_attachments_exclusive" "managed" {
+  role_name = aws_iam_role.assumable.name
+  policy_arns = compact(flatten([
+    [for policy in aws_iam_policy.main : policy.arn],
+    [for policy in data.aws_iam_policy.custom : policy.arn],
+  ]))
+}
 
-output "iam_assumable_role" {
-  value = var.enabled ? aws_iam_role.iam_assumable_role[0] : null
+output "role_name" {
+  value = aws_iam_role.assumable.name
+}
+
+output "role_unique_id" {
+  value = aws_iam_role.assumable.unique_id
+}
+
+# remove in a subsequent MR/release
+
+moved {
+  from = aws_iam_policy.iam_role_policy
+  to   = aws_iam_policy.main
+}
+
+moved {
+  from = aws_iam_role.iam_assumable_role[0]
+  to   = aws_iam_role.assumable
+}
+
+moved {
+  from = aws_iam_role_policy_attachment.policy_attachment_custom
+  to   = aws_iam_role_policy_attachment.custom
+}
+
+moved {
+  from = aws_iam_role_policy_attachment.policy_attachment_main
+  to   = aws_iam_role_policy_attachment.main
 }
